af61cb21f834a5ee7da4e1a42ed358e7cd2c38ef
Jose Antonio Carneiro Avila <jose.carneiro.avila@gmail.com>
2025-02-22 12:05:41 -0300

    contoller feito

---
+++ product-api/src/main/java/com/zezesheep/product_api/controller/ProductController.java
@@ -0,0 +1,63 @@
+package com.zezesheep.product_api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zezesheep.product_api.dto.ProductDTO;
+import com.zezesheep.product_api.service.ProductService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/product")
+public class ProductController {
+
+    private ProductService productService;
+
+    @GetMapping
+    public List<ProductDTO> getProducts(){
+        return productService.getAll();
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public List<ProductDTO> getProductByCategory(@PathVariable Long categoryId){
+        return productService.getByCategoryId(categoryId);
+    }
+
+    @GetMapping("/{productIdentifier}")
+    public ProductDTO findById(@PathVariable String productIdentifier){
+        return productService.findByProductIdentifier(productIdentifier);
+    }
+
+    @PostMapping
+    public ProductDTO newProduct(@Valid @RequestBody ProductDTO productDTO){
+        return productService.save(productDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        productService.delete(id);
+    }
+
+    @PostMapping("/{id}")
+    public ProductDTO editProduct(@PathVariable Long id, @RequestBody ProductDTO productDTO){
+        return productService.editProduct(id, productDTO);
+    }
+
+    @GetMapping("/pageable")
+    public Page<ProductDTO> getProductsPage(Pageable pageable){
+        return productService.getAllPage(pageable);
+    }
+}
